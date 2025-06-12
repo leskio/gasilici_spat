fa0ae24f734ef8171d336bff05d596f9a4a2f0e4
leskio <leskovar.aljaz@outlook.com>
2025-06-12 22:09:01 +0200

    nova oblika obrazca, gumb za dobrodošlico pokaže pozdrav glede na uro

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ function svetloOzadje() {
 function temnoOzadje() {
     document.body.style.backgroundColor = darkColor;
 }
-function dobrodosli(){
-    alert("Dobrodošli!!!")
+function dobrodosli() {
+    const hour = new Date().getHours();
+    
+    if (hour < 9) {
+        alert("Dobro jutro");
+    } else if (hour >= 9 && hour < 18) {
+        alert("Dober dan");
+    } else {
+        alert("Dober večer");
+    }
 }
-function prikaziAlert(){
-    alert("Obrazec poslan")
-}
+
 /* if document.getElementsById("gumbek") ===;
 naredi preverjanje pritiska gumba
 */
@@ -25,8 +31,10 @@ function preveriObrazec (){
     if (ime === "" || priimek === "" || email === "");
         alert("Prosim izpolnite vsa polja.");
         event.preventDefault(); // prepreči pošiljanje
+    // else ();
+    //     prikaziAlert()
 }
 
 window.onload = function() {
 document.querySelector("form").addEventListener("submit", preveriObrazec); //ne vem kaj to naredi, kopiral sem kodo
-};
+}
